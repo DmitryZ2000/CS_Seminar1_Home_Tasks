f072e3aca8a9f30cc8e20ca893a2a07e2921b1b3
DVZ <polo1215@bk.ru>
2022-08-09 00:44:33 +0300

    List of even numbers

---
+++ Task008/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите произвольное целое число больше 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+while (index < number)
+{
+    Console.Write($"{index}  ");
+    index = index + 2;
+}

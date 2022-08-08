16ad601659ca57abdb88960489c7e9c61c5d6c59
DVZ <polo1215@bk.ru>
2022-08-09 00:28:49 +0300

    Add Task006

---
+++ Task006/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите произвольное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+    Console.Write($"Число {number} является четным");
+else
+    Console.Write($"Число {number} является нечетным");

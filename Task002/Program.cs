0bdcd7b0f569349fa5441ce3b6b63e64810ddfb4
DVZ <polo1215@bk.ru>
2022-08-09 00:12:05 +0300

    Add Task2.release1

---
+++ Task002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+// int a = 53;
+// int b = 533;
+if (a > b)
+    Console.WriteLine($"Число {a} больше");
+else if ((a < b))    
+    Console.WriteLine($"Число {b} больше");
+else 
+    Console.WriteLine($"Числа равны");
